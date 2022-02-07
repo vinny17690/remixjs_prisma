bea0f319cabc919ab9e582c632c782d990d97ea7
Vinay <vinay.vinny18@gmail.com>
2022-02-07 08:50:55 -0500

    Form styles

--- app/routes/post/$postId.edit.tsx
+++ app/routes/post/$postId.edit.tsx
@@ -65,16 +65,24 @@ export default function PostEdit() {
 		<div className="post-edit-ontainer">
 			{errors && <span className="error-message">Your form submitted with errors. Please verify, and re-submit</span>}
 			<Form method="post">
-				<label htmlFor="title">Post Title:</label>
-				<input type="text" name="title" id="title" defaultValue={post.title} />
-				<label htmlFor="post">Post Content:</label>
-				<textarea defaultValue={post.post} id="post" name="post" />
-				<label htmlFor="post_status">Post Status:</label>
-				<select name="post_status" id="post_status" defaultValue={post.post_status}>
-					<option value="draft">Draft</option>
-					<option value="publish">Publish</option>
-				</select>
-				<button type="submit" value="submit">{transition.submission ? 'Updating..' : 'Update Post'}</button>
+				<div className="post-title-container">
+					<label htmlFor="title">Post Title:</label>
+					<input type="text" name="title" id="title" defaultValue={post.title} />
+				</div>
+				<div className="post-content-container">
+					<label htmlFor="post">Post Content:</label>
+					<textarea defaultValue={post.post} id="post" name="post" rows={15} cols={50} />
+				</div>
+				<div className="post-status-container">
+					<label htmlFor="post_status">Post Status:</label>
+					<select name="post_status" id="post_status" defaultValue={post.post_status}>
+						<option value="draft">Draft</option>
+						<option value="publish">Publish</option>
+					</select>
+				</div>
+				<div className="post-submit-container">
+					<button className="submit-button" type="submit" value="submit">{transition.submission ? 'Updating..' : 'Update Post'}</button>
+				</div>
 			</Form>
 		</div>
 	);
